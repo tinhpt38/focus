857e7c5b398c294d8c8aff20cd2b3797f2285de6
tinhpt <tinhpt.38@gmail.com>
2020-06-15 09:55:02 +0700

    add admin page and git ignore

---
+++ lib/ui/modules/admin/admin_page.dart
@@ -0,0 +1,37 @@
+
+import 'package:flutter/material.dart';
+import 'package:focus_app/ui/base/responsive.dart';
+
+
+
+class AdminPage extends StatefulWidget {
+  @override
+  _AdminPageState createState() => _AdminPageState();
+}
+
+class _AdminPageState extends State<AdminPage> with ResponsivePage{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+
+  @override
+  Widget buildDesktop(BuildContext context) {
+      // TODO: implement buildDesktop
+      throw UnimplementedError();
+    }
+  
+    @override
+    Widget buildMobile(BuildContext context) {
+      // TODO: implement buildMobile
+      throw UnimplementedError();
+    }
+  
+    @override
+    Widget buildTablet(BuildContext context) {
+    // TODO: implement buildTablet
+    throw UnimplementedError();
+  }
+}
---
+++ lib/ui/modules/admin/admin_page_model.dart
@@ -0,0 +1,15 @@
+
+
+
+import 'package:focus_app/core/services/chat_socket_io.dart';
+import 'package:focus_app/ui/base/base_page_model.dart';
+
+class AdminModel extends PageModel{
+
+  ChatSocketIO _chatSocketIO = ChatSocketIO();
+
+
+
+
+
+}
